bd016ac14753c7cd11a2ecac4d2316b7bcf6eb30
dzemlianoi <dzemlianoi@gmail.com>
2016-10-19 15:24:17 +0300

    ref done

--- index.rb
+++ index.rb
@@ -11,8 +11,6 @@ author5 = Author.new('M. Tven', 'Hello')
 author6 = Author.new('F. Kafka', 'Hello')
 author7 = Author.new('R. Somsmert', 'Hello')
 #
-library.add_authors(author1, author2, author3, author4, author5, author6, author7)
-#
 book = Book.new('Harry Potter I', author1)
 book1 = Book.new('Harry Potter II', author1)
 book2 = Book.new('Harry Potter III', author1)
@@ -23,14 +21,17 @@ book6 = Book.new('Game of thrones II', author4)
 book6 = Book.new('Tom Sawyer', author5)
 book7 = Book.new('All about us', author7)
 #
-library.add_books(book, book1, book2, book3, book4, book5, book6, book7)
-#
 reader1 = Reader.new('Denis', 'dzem@gmail.com', 'Dnipro', 'GS', 140)
 reader2 = Reader.new('Test', 'test@gmail.com', 'Kiev', 'KM', 101)
 reader3 = Reader.new('Testovskiy', 'testovskiy@gmail.com', 'Uzhgorod', 'KL', 22)
 reader4 = Reader.new('Testarius', 'testarius@gmail.com', 'Odessa', 'BH', 71)
 reader5 = Reader.new('Testevich', 'testarich@gmail.com', 'Kharkiv', 'YM', 41)
-library.add_readers(reader1, reader2, reader3, reader4, reader5)
+#
+library.add(
+    author1, author2, author3, author4, author5, author6, author7,
+    book, book1, book2, book3, book4, book5, book6, book7,
+    reader1, reader2, reader3, reader4, reader5
+)
 #
 order = Order.new(library.readers[0], library.books[0])
 order1 = Order.new(library.readers[4], library.books[1])
@@ -43,8 +44,9 @@ order7 = Order.new(library.readers[0], library.books[3])
 order8 = Order.new(library.readers[3], library.books[3])
 order9 = Order.new(library.readers[2], library.books[2])
 order10 = Order.new(library.readers[1], library.books[1])
+order11 = Order.new(library.readers[4], library.books[3])
 
-# library.add_orders(order,order1,order2,order3,order4,order5,order6,order7,order8,order9,order10)
+library.add(order1, order2, order3, order4, order5, order6, order7, order8, order9, order10)
 
 library.save('library')
 
--- lib/library.rb
+++ lib/library.rb
@@ -1,6 +1,7 @@
 class Library
   include Loader
   attr_accessor :authors, :readers, :books, :orders
+
   def initialize
     @authors = []
     @books = []
@@ -8,53 +9,21 @@ class Library
     @orders = []
   end
 
-  def add_books(*books)
-    books.each do |book|
-      @books.push(book) if check(book, Book, @books) && @authors.include?(book.author)
-    end
-  end
-
-  def add_authors(*authors)
-    authors.each do |author|
-      @authors.push(author) if check(author, Author, @authors)
-    end
-  end
-
-  def add_readers(*readers)
-    readers.each do |reader|
-      @readers.push(reader) if check(reader, Reader, @readers)
-    end
-  end
-
-  def add_orders(*orders)
-    orders.each do |order|
-      if @readers.include?(order.reader) && @books.include?(order.book)
-        @orders.push(order)
-      else
-        raise 'No such reader or Book in the Library'
+  def add(*items)
+    items.each do |item|
+      case item
+        when Book
+          @books.push(item) if !@books.include?(item) && @authors.include?(item.author)
+        when Order
+          @orders.push(item) if @readers.include?(item.reader) && @books.include?(item.book)
+        when Reader
+          @readers.push(item) unless @readers.include?(item)
+        when Author
+          @authors.push(item) unless @authors.include?(item)
+        else
+          raise 'Undefined element'
       end
     end
-  end
-
-  def check(item, type, collection)
-    item.is_a?(type) && !collection.include?(item)
-  end
-
-  def grouping_by(flag)
-    @orders.group_by do |order|
-      order.public_send(flag)
-    end
-  end
-
-  def sort_grouped(data)
-    sorted = data.sort_by{ |k,v| v.size }
-    sorted.reverse.to_h
-  end
-
-  def top_of(flag)
-    grouped_orders = grouping_by(flag)
-    sorted_orders = sort_grouped(grouped_orders)
-    sorted_orders.keys
   end
 
   def top_reader
@@ -66,15 +35,34 @@ class Library
   end
 
   def top_books_statistics(number)
-    top_books_readers = []
     raise 'Annormal quantity of books' if number > top_of(:book).count
+    top_books_readers = []
     first_n_books = top_of(:book).first(number)
-    books_data = grouping_by(:book)
+    groups_by_book = grouping_by(:book)
     first_n_books.each do |book|
-      books_data[book].each do |order|
+      groups_by_book[book].each do |order|
         top_books_readers.push(order.reader)
       end
     end
     top_books_readers.uniq.size
+  end
+
+  private
+
+  def grouping_by(flag)
+    @orders.group_by do |order|
+      order.public_send(flag)
+    end
+  end
+
+  def sort_grouped(data)
+    sorted = data.sort_by { |_, v| v.size }
+    sorted.reverse.to_h
+  end
+
+  def top_of(flag)
+    grouped_orders = grouping_by(flag)
+    sorted_orders = sort_grouped(grouped_orders)
+    sorted_orders.keys
   end
 end
